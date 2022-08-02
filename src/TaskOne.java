ba76d98c7a08d06ec26aea7dfe03e131bde0db95
Sarvar Masharipov <sarvarmasharipov72>
2022-08-02 11:29:57 +0500

    Algorithm completed

--- src/TaskOne.java
+++ src/TaskOne.java
@@ -1,6 +1,26 @@
 public class TaskOne {
-    public int[] decryptData(int[] price, int discount, int offset, int readLength) {
-        
+    public static int[] decryptData(int[] price, int discount, int offset, int readLength) {
+        if (price.length == 0) {
+            return price;
+        }
+        if (discount == 0) {
+            return price;
+        }
+        if (readLength < 1) {
+            return price;
+        }
+        if (offset >= price.length) {
+            return price;
+        }
+        if (offset + readLength > price.length) {
+            readLength = price.length - offset;
+        }
+        int[] newPrice = new int[readLength];
 
+        for (int i = 0; i < readLength; i++) {
+            newPrice[i] = (int) (price[offset + i] * (discount * 1.0 / 100));
+        }
+
+        return newPrice;
     }
 }
